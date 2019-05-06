88db5de124cc959cdbf397df97cf8fd9a3e9ec94
Franco Ribas <fdrserviceyventas@gmail.com>
2019-05-06 17:45:02 -0300

    lo basico esta

--- src/model/Incaa.java
+++ src/model/Incaa.java
@@ -1,5 +1,47 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;;
+
 public class Incaa {
+
+	private List<Pelicula> catalogo;
+
+	public Incaa(List<Pelicula> catalogo) {
+		super();
+		this.catalogo = catalogo;
+	}
+
+	public List<Pelicula> getCatalogo() {
+		return catalogo;
+	}
+
+	public void setCatalogo(List<Pelicula> catalogo) {
+		this.catalogo = catalogo;
+	}
+
+	@Override
+	public String toString() {
+		return "Incaa [catalogo=" + catalogo + "]";
+	}
+
+	public boolean agregarPelicula(String pelicula) throws Exception {
+		boolean agregado = false;
+		int indice = 0;
+		String nombrePelicula = pelicula.toLowerCase();
+		if (catalogo.isEmpty()) {
+			agregado = catalogo.add(new Pelicula(0, nombrePelicula));
+		} else {
+			//Conrtolo que la pelicula a agregar no este en la lista
+			while (!agregado && indice < catalogo.size()) {
+				
+				
+				
+				indice++;
+			}
+		}
+
+		return agregado;
+	}
 
 }
--- src/model/Pelicula.java
+++ src/model/Pelicula.java
@@ -1,5 +1,41 @@
 package model;
 
 public class Pelicula {
+	private int idPelicula;
+	private String pelicula;
 
+	public Pelicula(int idPelicula, String pelicula) throws Exception {
+		super();
+		this.idPelicula = idPelicula;
+		this.setPelicula(pelicula);
+	}
+
+	public int getIdPelicula() {
+		return idPelicula;
+	}
+
+	public void setIdPelicula(int idPelicula) {
+		this.idPelicula = idPelicula;
+	}
+
+	public String getPelicula() {
+		return pelicula;
+	}
+
+	public void setPelicula(String pelicula) {
+		String nombrePelicula = pelicula.toLowerCase();
+		this.pelicula = nombrePelicula;
+	}
+
+	@Override
+	public String toString() {
+		return "Incaa [idPelicula=" + idPelicula + ", pelicula=" + pelicula + "]";
+	}
+
+	public boolean equals(Pelicula pelicula) {
+		
+		return true;
+	}
+	
+	
 }
